b004697cd23ad50b313028c3871e67e1231d685b
A2S-Innovate-Tech <83775048+SankalpGupta2001@users.noreply.github.com>
2023-12-23 04:12:33 -0800

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path'); // Require the 'path' module
+require('dotenv').config();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+const cors = require('cors');
+const UsersControllers = require('./controllers/UsersControllers');
+const UsersDataControllers = require('./controllers/UsersDataControllers');
+
+
+const connectDB = require('./config/db');
+connectDB(); 
+app.use(cors());
+
+
+
+
+app.use('/app', UsersControllers);
+app.use('/app', UsersDataControllers);
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
